24333674d6eca813321762321d1e66c988b66a50
Tera Earlywine <teraearlywine@teras-mbp.lan>
2022-07-06 15:30:23 -0700

    Docs

--- hustle/target/compiled/hustle/models/staging__users.sql
+++ hustle/target/compiled/hustle/models/staging__users.sql
@@ -1,7 +1,7 @@
 
 
 
-SELECT  RANK() OVER (PARTITION BY created_dt ORDER BY created_dt ASC) AS id
-      , created_dt
+SELECT id
+     , created_dt
 
 FROM   `portfolio-351323`.`dbt_tera`.`staging__date`
--- hustle/target/run/hustle/models/staging__users.sql
+++ hustle/target/run/hustle/models/staging__users.sql
@@ -8,8 +8,8 @@
     
 
 
-SELECT  RANK() OVER (PARTITION BY created_dt ORDER BY created_dt ASC) AS id
-      , created_dt
+SELECT id
+     , created_dt
 
 FROM   `portfolio-351323`.`dbt_tera`.`staging__date`
   );
