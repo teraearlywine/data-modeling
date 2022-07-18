718e3f54bc5913b76e03cbbf3d207940bf08df5a
Tera Earlywine <teraearlywine22@gmail.com>
2022-07-18 16:13:34 -0700

    Refactor abstraction layer

--- hustle/models/Staging/staging__versions.sql
+++ hustle/models/Staging/staging__versions.sql
@@ -4,5 +4,10 @@
       )
 }}
 
-SELECT  *
+SELECT  id
+      , first_name
+      , last_name
+      , DATE(created_ts) AS created_dt
+      , family_role
+
 FROM   `portfolio-351323.dev_tera.family_members`
