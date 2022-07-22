957f26c7e8bc2cfc1c410111ee86d0622048a011
Tera Earlywine <teraearlywine22@gmail.com>
2022-07-22 15:55:32 -0700

    rename

--- hustle/scripts/databse_family/backend/dev__family_hobbies.sql
+++
@@ -1,8 +0,0 @@
-
-
--- Replace values for hobby_type & update family member id
--- Execute when ready
--- Easier to populate than doing a procedure with calls 
-INSERT INTO family_hobbies(hobby_type, created_ts, family_id)
-VALUES('State of things', CURRENT_TIMESTAMP, (SELECT id FROM family WHERE id = 9));
-
